38691096193f9da9c929f027a00377815a96ef7a
Wojciech <wojciechwroobel@gmail.com>
2024-08-15 21:35:24 +0200

    finished ex2 - paragraph changing its font size depending on the input

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 1
+
 const backgroundChangingButton = document.querySelector('#background-changing-button');
 const colorInput = document.querySelector('#color-input');
 
@@ -5,5 +7,20 @@ if (backgroundChangingButton && colorInput) {
     backgroundChangingButton.addEventListener('click', function() {
         const inputText = colorInput.value;
         backgroundChangingButton.style.backgroundColor = inputText;
+    })
+}
+
+// 2
+
+const fontSizeInput = document.querySelector('#font-size-input');
+const fontSizeParagraph = document.querySelector('#font-size-paragraph');
+
+if (fontSizeInput) {
+    fontSizeInput.addEventListener('input', function() {
+        let inputNumber = fontSizeInput.value;
+        if (inputNumber <= 0) {
+            inputNumber = 12;
+        }
+        fontSizeParagraph.style.fontSize = `${inputNumber}px`;
     })
 }
